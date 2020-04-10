cd8be5be2a1b5dd5cf23467849d0b73eeec4fcc9
Clayton Dean <clay_holman8@yahoo.com>
2020-04-10 14:59:22 -0700

    Updates

---
+++ precache-manifest.23f9e8d3a3c97a845d820d0568e1b265.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ff78b90819593773415e9a2d85ea785",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5759e9973c075f5551b6",
+    "url": "/static/js/2.2ffaef17.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2ffaef17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61eb920c03a4b3003f76",
+    "url": "/static/js/main.dc0794d3.chunk.js"
+  },
+  {
+    "revision": "126503d1ad86592f7968",
+    "url": "/static/js/runtime-main.098a087d.js"
+  }
+]);
