9f7ccf5f216be0769a87623aef4ddea71aabf555
Clayton Dean <clay_holman8@yahoo.com>
2020-04-10 01:11:12 -0700

    Updates

---
+++ precache-manifest.18cc19f390c08382915ba1fad77985e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b4bd491c80493f91da96c4ef8367b47",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "5759e9973c075f5551b6",
+    "url": "/portfolio-react/static/js/2.2ffaef17.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio-react/static/js/2.2ffaef17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61eb920c03a4b3003f76",
+    "url": "/portfolio-react/static/js/main.dc0794d3.chunk.js"
+  },
+  {
+    "revision": "4c3bcdd375f26df9b5b7",
+    "url": "/portfolio-react/static/js/runtime-main.70e43ac6.js"
+  }
+]);
